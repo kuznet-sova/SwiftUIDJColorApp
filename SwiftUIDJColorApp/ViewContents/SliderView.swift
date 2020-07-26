f483fa2bee6d3bb3de3266b42995c38e649c96b7
kuznet.i.sova <kis7425910@gmail.com>
2020-07-26 11:12:52 +0300

    added data transfer from the text field to slider value

--- SwiftUIDJColorApp/ViewContents/SliderView.swift
+++ SwiftUIDJColorApp/ViewContents/SliderView.swift
@@ -34,11 +34,13 @@ struct SliderStack: View {
                 .foregroundColor(.white)
                 .frame(width: 50, height: 35)
             
-            Slider(value: $sliderValue, in: 0...255, step: 1){_ in
+            Slider(value: $sliderValue, in: 0...255, step: 1) {_ in
                 self.textFieldValue = "\(Int(self.sliderValue))"
             }
             
-            TextField("0", text: $textFieldValue)
+            TextField("0", text: $textFieldValue) {
+                self.sliderValue = Double(self.textFieldValue) ?? 0
+            }
                 .frame(width: 50, height: 35)
                 .foregroundColor(.black)
                 .padding(.leading, 8)
